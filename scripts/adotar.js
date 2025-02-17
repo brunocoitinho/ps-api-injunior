b42df947b90f2bc1334d7103c7377dac9fbe9c86
Yasmin Macedo <yasminmacedo_11@outlook.com>
2025-02-17 15:36:24 -0300

    atualização da page adotar-lobo

--- scripts/adotar.js
+++ scripts/adotar.js
@@ -5,26 +5,46 @@ let lobinhos = JSON.parse(localStorage.getItem('lobos'))
 adotarBtn.addEventListener('click', ()=>{
     let index = lobinhos.findIndex(x => x.id == loboSelecionado.id);
     console.log(index)
-    lobinhos[index] = adotarNovoLobo(lobinhos[index])
-    localStorage.setItem("lobos", JSON.stringify(lobinhos))
-    window.location.replace("lista-de-lobinhos.html");
+    adotarNovoLobo(index)
+    //localStorage.setItem("lobos", JSON.stringify(lobinhos))
+    //window.location.replace("lista-de-lobinhos.html");
 })
 
 
-function adotarNovoLobo(lobo) {
+function adotarNovoLobo(loboIndex) {
 
-    let novoLobo = lobo
+    let id = loboIndex
     let _nomeDono = document.querySelector('#nome-dono');
     let _idadeDono = document.querySelector('#idade-dono');
     let _emailDono = document.querySelector('#email-dono');
-    let nomeDono = _nomeDono.value.trim();
-    let idadeDono = parseInt(_idadeDono.value.trim(), 10);
-    let emailDono = _emailDono.value.trim();
 
-    novoLobo.nomeDono = nomeDono
-    novoLobo.idadeDono = idadeDono
-    novoLobo.emailDono = emailDono
-    novoLobo.adotado = true
-    
-    return novoLobo
+    const patch = {
+        nomeDono: _nomeDono.value.trim(),
+        idadeDono: parseInt(_idadeDono.value.trim(), 10),
+        emailDono: _emailDono.value.trim(),
+        adotado: true
+    }
+
+    alterarStatus(patch, id)
+}
+
+
+async function alterarStatus(patch, id) {
+    try {
+        const resposta = await fetch(`https://localhost:3000/lobos/${id}`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(patch),
+        })
+        if (!resposta.ok) {
+            throw new Error(`Erro: ${resposta.status}`);
+        }
+        const dados = await resposta.json()
+        console.log(dados)
+
+    } catch (error) {
+        console.log(error.message)
+    }
 }
